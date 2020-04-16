374374d9793a88c9fe179a08d6a6a9a1bfb1925b
pawel.koselski <pawel.koselski@aptitudesoftware.com>
2020-04-16 09:27:04 +0200

    Use StringBuilder to boost performance by more than 10%

--- Solution.cs
+++ Solution.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace IntegerToRoman
 {
     public class Solution
@@ -7,19 +9,19 @@ namespace IntegerToRoman
             int[] numbers = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
             string[] symbols = new string[] { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
             int index = 0;
-            string retVal = "";
+            StringBuilder sb = new StringBuilder();
             while (index < 13)  //arrays size
             {
                 int div = num / numbers[index];
                 for (int i = 0; i < div; i++)
-                    retVal += symbols[index];
+                    sb.Append(symbols[index]);
 
                 num = num % numbers[index];
 
                 index++;
             }
 
-            return retVal;
+            return sb.ToString();
         }
     }
 }
